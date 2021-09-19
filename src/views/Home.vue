7091a10c7b26ff34f06b27e2f2d8a88785ea2456
xcrn <xcrnvieira@gmail.com>
2021-09-19 17:34:17 -0300

    firts commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,28 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Saudações!</ion-title>
       </ion-toolbar>
     </ion-header>
     
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-title size="large">Olá!</ion-title>
         </ion-toolbar>
       </ion-header>
     
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+    <div class="body"> 
+      <form @submit.prevent="horario">
+        <div>
+          <label>Digite a hora que está mostrando na sua tela: (__h)</label>
+        </div>
+        <input v-model.number="hora" type="number"/>
+        <ion-button type="submit">Enviar</ion-button>
+      </form>
+      
+      <span> {{ mensagem }} </span>
+    </div>
     </ion-content>
   </ion-page>
 </template>
@@ -27,6 +34,32 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Home',
+  data() {
+    return {
+      hora: '', 
+      mensagem: ''
+    };
+  },
+  methods: {
+    horario() {
+      const {hora} = this;
+      if(hora>= 0 && hora <= 5){
+        this.mensagem = 'Boa noite';
+      }
+      if (hora>=6 && hora<= 12) { 
+        this.mensagem = 'Bom dia';
+      }
+      if(hora>12 && hora<=18){
+        this.mensagem = 'Boa tarde';
+      }
+      if (hora>=19 && hora <= 23) {  
+        this.mensagem = 'Boa noite';
+      }
+      if (hora > 24){
+         this.mensagem = 'Digite uma hora válida.';
+      }
+    }
+  },
   components: {
     IonContent,
     IonHeader,
@@ -38,6 +71,24 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.body {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  justify-content: center;
+  padding: 40px;
+  background-color: rgb(121, 172, 238);
+  height: 100vh;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+label {
+  margin: 5px 0;
+}
+input { 
+  margin: 10px;
+}
+
 #container {
   text-align: center;
   
